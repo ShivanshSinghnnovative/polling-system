8b153add7c06549ea86543fe4e4b83161418c68e
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-10-27 12:59:46 +0530

    update the logout emit

--- src/components/logoutModal.vue
+++ src/components/logoutModal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=" inset-0 bg-black absolute min-h-screen  bg-opacity-50" @click="closeMenu">
+    <div class=" inset-0 bg-black absolute min-h-screen  bg-opacity-50" @click="$emit('closeModal')">
         <div class="bg-gray-200  bg-opacity-.5 absolute mt-20 right-2" @click.stop>
             <button
                 class="border-black  text-xl bg-green-400 text-center flex  m-5 justify-center lg:text-2xl sm:text-base  md:text-xl text-black mv:text-xs text-md rounded-md pl-4 p-1 font-medium pr-4"
@@ -12,7 +12,7 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import { defineEmits } from 'vue';
-const emit = defineEmits({});
+defineEmits(['closeModal']);
 const router = useRouter()
 import { useStore } from 'vuex';
 const store = useStore()
@@ -23,7 +23,5 @@ const logoutUser = async () => {
     await store.commit('setUserDetails')
     router.push('/')
 }
-const closeMenu = () => {
-    emit('closeModal');
-};
+
 </script>
